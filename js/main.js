2ae192224ee5033b7698e01d24cdc03635844961
Akuby <akuby1997@gmail.com>
2022-10-21 03:36:01 +0900

    1차 js 구현

--- js/main.js
+++ js/main.js
@@ -1,62 +1,133 @@
-const menu = document.getElementById('lnb');
-const nav = document.querySelector('header');
-// 네비게이션
-let menuRollDown = menu.addEventListener('mouseenter', function () {
-  nav.style.height = '20rem';
-});
-let menuRollUp = menu.addEventListener('mouseleave', function () {
-  nav.style.height = '6rem';
-})
+let resizeCount = 0
+let isMobile = 0;
+let missingperson = $('#miss-slider li').get();
+let cooperate = $('#coop-slider li a').get();
 
-function resize() {
-  console.log('resized')
+function insertBg(sliderName, folderName) {
+  sliderName.forEach((el, idx) => {
+    $(el).css({
+      backgroundImage: `url(./assets/${folderName}/${idx+1}.png)`
+    })
+  });
 }
 
-$(window).on('resize', function() {
-  if($(window).innerWidth() <= 768) {
-    resize();
+function resize() {
+  console.log('resized : ' + resizeCount++)
+  if ($(window).innerWidth() <= 768) {
+    isMobile = 1;
+  } else {
+    isMobile = 0;
   }
-});
+}
 
-// // 캠페인 슬라이더 in js
-// const campNextBtn = document.querySelector('#camp-next');
-// const campPrevBtn = document.querySelector('#camp-prev');
-// const campSlider = document.querySelector('#camp-slider');
-// let campLength = campSlider.childElementCount;
-// const slideSpeed = 400;
-// // 슬라이더 넓이 동적 지정 및 트랜지션 속도 지정
-// campSlider.style.width = `${(campLength) * 100}%`;
-// campSlider.style.transition = `transform ${slideSpeed}ms`;
+insertBg(missingperson, 'missingperson');
+insertBg(cooperate, 'cooperate');
+resize();
+$(window).on('resize', resize);
 
-// // curIndex를 통해 
-// let curIndex = 1;
-// campNextBtn.addEventListener('click', function () {
-//   if (curIndex == campLength) {
-//     let dupNode = campSlider.firstElementChild.cloneNode(true);
-//     // 1번 복제해서 마지막 뒤에 append 해준 다음
-//     campSlider.appendChild(dupNode);
-//     campLength = campSlider.childElementCount;
-//     campSlider.style.width = `${(campLength) * 100}%`;
-//     // 현재 위치 조정해주고
-//     campSlider.style.transition = `0ms`;
-//     campSlider.style.transform = `translateX(-${(100 / campLength) * (curIndex-1)}%)`;
-//     // 똑같이 translate 하는거 보여주고,
-//     setTimeout(() => {
-//       campSlider.style.transition = `transform ${slideSpeed}ms`;
-//       campSlider.style.transform = `translateX(-${(100 / campLength) * (curIndex)}%)`;
-//     }, 1);
-//     campSlider.style.transition = `0ms`;
-//     campSlider.style.transform = `translateX(-0%)`
-//     // append한거 지운다음 애니메이션 없이 translate 0 한다.
-//     // curIndex = 1;
-//   } else {
-//     campSlider.style.transform = `translateX(-${(100 / campLength) * (curIndex)}%)`;
-//     curIndex++;
-//   }
-//   // curSlide = slideContents[++curIndex];
+
+// 네비게이션
+// debounce 적용하고 나서는 resize에 바로 넣기
+if (isMobile == 0) {
+  $('#lnb').on('mouseenter', function () {
+    $('header').css({
+      height: '20rem'
+    })
+  }).on('mouseleave', function () {
+    $('header').css({
+      height: '6rem'
+    })
+  })
+}
+if (isMobile == 1) {
+  $('#campaign').toggleClass('min');
+}
+
+$('#navi-show').on('click', function () {
+  if($('header').hasClass('min')) { // 닫기
+    $('#lnb, #gnb').fadeOut(200, function() {
+      $('header').removeClass('min');
+    });
+  } else { // 열기
+    $('header').addClass('min'); 
+    $('#lnb, #gnb').hide().fadeIn(200);
+  }
+})
+
+// 실종아동 슬라이더
+let initMargin;
+let sliderMarginLeft = parseInt($('#camp-slider').css('marginLeft'));
+let temp = 0;
+let isMoving = 0;
+$('#camp-next').on('click', function () {
+  if (isMoving == 0) {
+    isMoving = 1;
+    temp++;
+    $('#camp-slider').stop().animate({
+      marginLeft: `${-100 * temp}%`
+    }, 500, function () {
+      isMoving = 0
+    })
+  }
+})
+$('#camp-prev').on('click', function () {
+  if (isMoving == 0) {
+    isMoving = 1;
+    temp--;
+    $('#camp-slider').stop().animate({
+      marginLeft: `${-100 * temp}%`
+    }, 500, function () {
+      isMoving = 0;
+    })
+  }
+})
+$('#miss-next').on('click', function () {
+  $('#miss-slider').animate({
+    marginLeft: '-100%'
+  }, 500)
+})
+// $('#miss-prev').on('click', function() {
+//   $('#miss-slider').animate({marginLeft: `${}%`}, 500)
 // })
-// campPrevBtn.addEventListener('click', function () {
-//   // 마지막 슬라이드 복제해서 맨 앞에 prepend 해준 다음ㅁ
-//   // 똑같이 translate + 하는거 보여주고
-//   // prepend 한거 지운다음 애니메이션 없이 translate 66% 한다.
-// })
+
+
+
+// 모바일 터치 시험
+
+let curPos = 0;
+let postion = 0;
+let start_x, end_x;
+const IMAGE_WIDTH = 375;
+const images = document.querySelector(".slide");
+const campSlider = document.getElementById('camp-slider');
+ 
+images.addEventListener('touchstart', touch_start);
+images.addEventListener('touchend', touch_end);
+ 
+function prev(){
+  if(curPos > 0){
+    postion += IMAGE_WIDTH;
+    campSlider.style.transform = `translateX(${postion}px)`;
+    curPos = curPos - 1;
+  }
+}
+function next(){
+  if(curPos < 3){
+    postion -= IMAGE_WIDTH;
+    campSlider.style.transform = `translateX(${postion}px)`;
+    curPos = curPos + 1;
+  }
+}
+ 
+function touch_start(event) {
+  start_x = event.touches[0].pageX
+}
+ 
+function touch_end(event) {
+  end_x = event.changedTouches[0].pageX;
+  if(start_x > end_x){
+    next();
+  }else{
+    prev();
+  }
+}
